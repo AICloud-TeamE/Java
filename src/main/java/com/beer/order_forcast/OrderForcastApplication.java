a6906eca1f366c91f5eb1a17fac49b7fac9b1c95
KaiyuanZhang <kaiyuanzhang915@gmial.com>
2025-06-16 16:12:01 +0900

    first commit by Zhang

---
+++ src/main/java/com/beer/order_forcast/OrderForcastApplication.java
@@ -0,0 +1,13 @@
+package com.beer.order_forcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderForcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderForcastApplication.class, args);
+	}
+
+}
