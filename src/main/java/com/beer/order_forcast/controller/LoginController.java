6e313ef9d8222966412ba109f2711ca366bef1a0
KaiyuanZhang <kaiyuanzhang915@gmial.com>
2025-06-20 14:38:26 +0900

    zhang editted at 06/20 14:38

--- src/main/java/com/beer/order_forcast/controller/LoginController.java
+++ src/main/java/com/beer/order_forcast/controller/LoginController.java
@@ -41,6 +41,10 @@ public class LoginController {
             System.out.println("管理者として");
             return "redirect:/history_all";
         } else if (result == 4) {
+            Account account = accountService.getAccountByEmail(userId);
+            session.setAttribute("userId", account.getId());
+            session.setAttribute("isAdmin", account.isAdmin());
+            model.addAttribute("isAdmin", account.isAdmin());
             System.out.println("一般ユーザーとして");
             return "redirect:/sales_input";
         } else {
